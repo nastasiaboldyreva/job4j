ff569e63e95ba3a1701cb75fbeb560ad673abf82
Anastasia Boldyreva <a93357@gmail.com>
2019-09-18 22:24:04 +0300

    Поиск индекса в диапазоне.[#159535]

---
+++ chapter_001/src/main/java/ru/job4j/array/FindLoopIndex.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+public class FindLoopIndex {
+    public static int indexOf(int[] data, int el) {
+        int rst = -1;
+        for (int i=0; i<data.length; i++) {
+            if(data[i]==el) {
+                rst = i;
+                break;
+            }
+        }
+        return rst;
+    }
+
+    public int indexOf(int[] data, int el, int start, int finish) {
+        int rst = -1;
+        for (int i=start; i<finish; i++) {
+            rst = data[el];
+        }
+        return rst;
+    }
+}
