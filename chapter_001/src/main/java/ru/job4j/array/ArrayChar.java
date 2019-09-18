1c7ee4aa0d641459ea7d3dcfe56d419de8bae154
Anastasia Boldyreva <a93357@gmail.com>
2019-09-18 20:06:52 +0300

    Слово начинается с ...[#159526]

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -3,11 +3,12 @@ package ru.job4j.array;
 public class ArrayChar {
 
     public static boolean startsW(char[] word, char[] pref) {
-        boolean result = false;
-        for (int i=0; i!=word.length; i++) {
-            for (int j=0; j!=pref.length; j++) {
-                if (word[i]==pref[j]) {
-                    result = true;
+        boolean result = true;
+        for (int i=0; i<word.length; i++) {
+            for (int j=0; j<pref.length; j++) {
+                if (word[j]!=pref[j]) {
+                    result = false;
+                    break;
                 }
             }
         }
