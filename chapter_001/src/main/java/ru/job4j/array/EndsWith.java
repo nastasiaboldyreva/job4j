8a7609415785601c7c16a43d96d186fbfbc14280
Anastasia Boldyreva <a93357@gmail.com>
2019-09-22 21:38:23 +0300

    Слово заканчивается с ...[#159534]

--- chapter_001/src/main/java/ru/job4j/array/EndsWith.java
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -4,14 +4,12 @@ public class EndsWith {
 
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
-        for (int i=0; i<word.length; i++) {
             for (int j=0; j<post.length; j++) {
                 if (word[word.length-1-j]!=post[post.length-1-j]) {
                 result = false;
                 break;
                 }
             }
-        }
 
         return result;
     }
