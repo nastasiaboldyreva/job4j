125c2cb8c395bf2241ac51bd1b3983d381eaa831
Anastasia Boldyreva <a93357@gmail.com>
2019-09-24 23:22:46 +0300

    Сортировка выборкой.[#159536]

--- chapter_001/src/main/java/ru/job4j/array/SortSelected.java
+++ chapter_001/src/main/java/ru/job4j/array/SortSelected.java
@@ -5,6 +5,10 @@ public class SortSelected {
     public static int[] sort(int[] data) {
         int min = MinDiapason.findMin(data, 0, data.length);
         int index = FindLoopIndex.indexOf(data, min, 0, data.length);
+        for(int i=0;i<data.length;i++) {
+            data[0]=min;
+
+        }
 
 
         return data;
