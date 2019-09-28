06f18be0d329f96e99c1854f84eeee4a4fc7d3f7
Anastasia Boldyreva <a93357@gmail.com>
2019-09-28 17:23:51 +0300

    Сортировка выборкой.[#159536]

--- chapter_001/src/main/java/ru/job4j/array/SortSelected.java
+++ chapter_001/src/main/java/ru/job4j/array/SortSelected.java
@@ -3,26 +3,16 @@ package ru.job4j.array;
 public class SortSelected {
 
     public static int[] sort(int[] data) {
-
-        for (int i=0;i<data.length;i++) {
-            int min = MinDiapason.findMin(data, 0, data.length);
-            int index = FindLoopIndex.indexOf(data, min, 0, data.length);
-            data[i]=min;
-            index=i;
-            int tmp=data[0];
-            data[0]=data[i];
-            data[i]=tmp;
-
-            for (int j=1; j<data.length-1;j++) {
-                if(data[j]<min) {
-                    min=data[j];
-                    index=j;
-                }
-                int tmp2=data[j];
-                data[j]=data[index];
-                data[index]=tmp2;
+        for (int i = 0; i < data.length; i++) {
+            int min = MinDiapason.findMin(data, i, data.length);
+            int index = FindLoopIndex.indexOf(data, min, i, data.length);
+            if (index== -1) {
+                break;
+            } else {
+                int t = data[i];
+                data[i] = data[index];
+                data[index] = t;
             }
-        }
-        return data;
+        } return data;
     }
 }
