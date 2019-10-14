bbb2789a304f382b26626ee9be1f246f69beb431
Anastasia Boldyreva <a93357@gmail.com>
2019-10-14 08:05:18 +0300

    Объединить два массива [#124482]

--- chapter_001/src/main/java/ru/job4j/sort/Merge.java
+++ chapter_001/src/main/java/ru/job4j/sort/Merge.java
@@ -6,22 +6,26 @@ public class Merge {
 
     public int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
-        for (int i=0; i < rsl.length; i++){
-            if (left[0] < right[0]) {
-                rsl[0] = left[0];
-            } else if (right[0] < left[0]){
-                rsl[0] = right[0];
+        for (int i=0; i < rsl.length; i++) {
+            for (int j=0; j < left.length; j++) {
+                for (int k=0; k < right.length; k++) {
+                    if (left[k] <= right[k]) {
+                        rsl[k] = left[k];
+                    } else if (right[k] < left[k]){
+                        rsl[k] = right[k];
+                    }
+                    while (right[k] > left[left.length-1]) {
+                        right[k]++;
+                        rsl[k]=right[k];
+                    }
+                    while (left[k] > right[right.length-1]) {
+                        left[k]++;
+                        rsl[k]=left[k];
+                    }
+                }
             }
-            if ((left[i]<rsl[i])&(left[i]<right[i])) {
-                rsl[i]=left[i];
-            }
-            else if ((right[i]<rsl[i])&(right[i]<left[i])) {
-                rsl[i]=right[i];
-            }
-            else if ((rsl[i]<left[i])&(rsl[i]<right[i])) {
-                rsl[i]++;
-            }
-        }return rsl;
+        }
+        return rsl;
     }
 
     public static void main(String[] args) {
