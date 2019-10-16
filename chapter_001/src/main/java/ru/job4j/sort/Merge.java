bba44bfcadbf226beb40ec7ee19ebad9f5546346
Anastasia Boldyreva <a93357@gmail.com>
2019-10-16 20:46:28 +0300

    Объединить два массива [#124482]

--- chapter_001/src/main/java/ru/job4j/sort/Merge.java
+++ chapter_001/src/main/java/ru/job4j/sort/Merge.java
@@ -6,33 +6,20 @@ public class Merge {
 
     public int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
-
-
-        for (int i=0; i < rsl.length; i++) {
-            for (int j=0; j < left.length; j++) {
-                for (int k=0; k < right.length; k++) {
-                    if (left[k] <= right[k]) {
-                        rsl[k] = left[k];
-                    } else if (right[k] < left[k]){
-                        rsl[k] = right[k];
-                    }
-
-                    if ((right.length-1) < (left.length-1)) {
-                        left[k]=k+1;
-
-
-                    }
-                    else if ((left.length-1) < (right.length-1)) {
-                        right[k]=k+1;
-
-                    }
-
-                }
+        int leftPoint = 0;
+        int rightPoint = 0;
+        int rslPoint = 0;
+        while (leftPoint + rightPoint != rsl.length) {
+            if (leftPoint != left.length && rightPoint != right.length) {
+                rsl[rslPoint++] = left[leftPoint] < right[rightPoint] ? left[leftPoint++] : right[rightPoint++];
+            } else if (leftPoint != left.length) {
+                rsl[rslPoint++] = left[leftPoint++];
+            } else {
+                rsl[rslPoint++] = right[rightPoint++];
             }
         }
         return rsl;
     }
-
     public static void main(String[] args) {
         Merge process = new Merge();
         int[] rsl = process.merge(
