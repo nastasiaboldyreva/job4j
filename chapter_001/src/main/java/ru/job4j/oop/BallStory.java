8c2caa238570dc5806310f1ada35aae5aa524169
Anastasia Boldyreva <a93357@gmail.com>
2019-10-18 20:53:31 +0300

    Взаимодействие объектов. [#166087]

---
+++ chapter_001/src/main/java/ru/job4j/oop/BallStory.java
@@ -0,0 +1,19 @@
+package ru.job4j.oop;
+
+import java.time.format.FormatStyle;
+
+public class BallStory {
+
+    public static void main (String[] args) {
+        Ball ball = new Ball();
+        Hare hare = new Hare();
+        WolfBall wolfball = new WolfBall();
+        Fox fox = new Fox();
+
+        ball.run(hare);
+        hare.tryEat(ball);
+        wolfball.tryEat(ball);
+        fox.tryEat(ball);
+
+    }
+}
