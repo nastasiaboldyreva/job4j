639d2ec63c2f9f2a25a8d64eb007c03d321089e5
Anastasia Boldyreva <a93357@gmail.com>
2019-10-22 21:32:18 +0300

    Рефакторинг - Площадь треугольника. [#122642]

--- chapter_001/src/main/java/ru/job4j/condition/TriangleOop.java
+++ chapter_001/src/main/java/ru/job4j/condition/TriangleOop.java
@@ -13,15 +13,14 @@ public class TriangleOop {
     }
 
     public double period(double a, double b, double c) {
-        double p = (a + b + c) / 2;
-        return p;
+        return -1;
     }
 
     public double area() {
         double rsl = -1;
         double a = first.distance(second);
-        double b = first.distance(third);
-        double c = second.distance(third);
+        double b = second.distance(third);
+        double c = first.distance(third);
         double p = period(a, b, c);
         if (this.exist(a, b, c)) {
             rsl = Math.sqrt(p * (p - a) * (p - b) * (p - c));
@@ -30,7 +29,7 @@ public class TriangleOop {
     }
 
     private boolean exist(double a, double c, double b) {
-        return a + b > c && b + c > a && a + c > a;
+        return false;
     }
 
 }
