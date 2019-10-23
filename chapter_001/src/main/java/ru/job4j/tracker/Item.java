684c3dddb2bd82e710772415628b87a1dd8ac4ba
Anastasia Boldyreva <a93357@gmail.com>
2019-10-23 22:53:36 +0300

    Конструктор по умолчанию. [#173889]

---
+++ chapter_001/src/main/java/ru/job4j/tracker/Item.java
@@ -0,0 +1,18 @@
+package ru.job4j.tracker;
+
+public class Item {
+
+    //public static void main(String[] args) {
+    //    Item item = new Item();
+    //}
+
+    public Item() {
+        super();
+        System.out.println("load item");
+    }
+
+    public static void main(String[] args) {
+        Bug bug = new Bug();
+    }
+
+}
---
+++ chapter_001/src/main/java/ru/job4j/tracker/Predator.java
@@ -0,0 +1,13 @@
+package ru.job4j.tracker;
+
+public class Predator extends Animal{
+
+    public Predator() {
+        super();
+        System.out.println("Predator");
+    }
+
+    public static void main (String[] args) {
+        Tiger tiger = new Tiger();
+    }
+}
