527a1805bc01e4db8c3a1247c536ad074d587a15
Anastasia Boldyreva <a93357@gmail.com>
2019-10-27 23:25:52 +0300

    Реализация профессий в коде [#6837]

---
+++ chapter_001/src/main/java/ru/job4j/tracker/Doctor.java
@@ -0,0 +1,11 @@
+package ru.job4j.tracker;
+
+public class Doctor extends Profession {
+
+    public Doctor (String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+
+    }
+
+}
