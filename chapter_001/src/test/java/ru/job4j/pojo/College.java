87f6681cf9bcd8b937fec33ef3281766311a0290
Anastasia Boldyreva <a93357@gmail.com>
2019-10-30 07:13:31 +0300

    Модель данных. [#174683]

---
+++ chapter_001/src/test/java/ru/job4j/pojo/College.java
@@ -0,0 +1,17 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class College {
+
+    public static void main (String[] args) {
+        Student student = new Student();
+        student.setName("Вася, Даша, Саша");
+        student.setGroup("a, b, c");
+        student.setEnter(new Date());
+
+        System.out.println(student.getName() + "  study university in groups  " + student.getGroup());
+
+
+    }
+}
