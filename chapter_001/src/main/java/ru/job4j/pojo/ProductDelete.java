d8a723ed2f95ae5f460ea44ed6ae7a38408df7f2
Anastasia Boldyreva <a93357@gmail.com>
2019-10-30 22:48:01 +0300

    Удаление моделей из массива. [#176470]

---
+++ chapter_001/src/main/java/ru/job4j/pojo/ProductDelete.java
@@ -0,0 +1,28 @@
+package ru.job4j.pojo;
+
+public class ProductDelete {
+
+    private String name;
+    private int count;
+
+    public ProductDelete ( String name, int count) {
+        this.name = name;
+        this.count = count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/pojo/Shop.java
@@ -0,0 +1,62 @@
+package ru.job4j.pojo;
+
+public class Shop {
+
+    public static void main(String[] args) {
+        ProductDelete productsdelete[] = new ProductDelete[5];
+        productsdelete[0] = new ProductDelete("Milk", 10);
+        productsdelete[1] = new ProductDelete("Bread", 4);
+        productsdelete[2] = new ProductDelete("Egg", 19);
+
+        for (int i = 0; i < productsdelete.length; i++) {
+            ProductDelete product = productsdelete[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+        System.out.println();
+        System.out.println("Удаляем значение из ячейки с индексом 1");
+        productsdelete[1] = null;
+
+        for (int i = 0; i < productsdelete.length; i++) {
+            ProductDelete product = productsdelete[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+        System.out.println();
+        System.out.println("Записываем в ячейку с индексом 1 значение ячейки с индексом 2 и удаляем значение из ячейки с индексом 2");
+        productsdelete[1] = productsdelete[2];
+        productsdelete[2] = null;
+        for (int i = 0; i < productsdelete.length; i++) {
+            ProductDelete product = productsdelete[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+    }
+
+        public ProductDelete[] delete(ProductDelete[] productsdelete, int index) {
+
+            System.out.println();
+            System.out.println("Заменим ячейку с индексом index на нулевую ссылку");
+            productsdelete[index] = null;
+            productsdelete[index] = productsdelete[index + 1];
+            for (int i = 0; i < productsdelete.length; i ++) {
+                ProductDelete product = productsdelete[i];
+                if (product != null) {
+                    System.out.println(product.getName());
+                } else {
+                    System.out.println("null");
+                }
+            }
+        }
+}
