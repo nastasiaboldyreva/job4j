fa51db6194d7cd98e2c5d867fcb73d262ed501f9
Anastasia Boldyreva <a93357@gmail.com>
2019-11-04 20:37:23 +0300

    Приведение типов. Повышение типа и понижение типа. [#181322]

---
+++ chapter_001/src/main/java/ru/job4j/oop/TypeCollege.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class TypeCollege {
+    public static void main (String[] args) {
+        TypeFreshman typefreshman = new TypeFreshman();
+        TypeStudent ts = typefreshman;
+        Object obj = typefreshman;
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/oop/TypeFreshman.java
@@ -0,0 +1,4 @@
+package ru.job4j.oop;
+
+public class TypeFreshman extends TypeStudent {
+}
