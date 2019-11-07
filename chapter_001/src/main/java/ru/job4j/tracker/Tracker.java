f64b4c78c798b6812989df5e04aa2660e692e9e6
Anastasia Boldyreva <a93357@gmail.com>
2019-11-07 07:30:29 +0300

    Реализовать класс Tracker [#396]

--- chapter_001/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_001/src/main/java/ru/job4j/tracker/Tracker.java
@@ -20,29 +20,54 @@ public class Tracker {
     }
     //        2-редактирование заявок - public boolean replace(String id, Item item);
     public boolean replace(String id, ItemTracker itemtracker) {
-
-//        for (int i=0; i < items.length; i++) {
-//            this.items[i] = items.setId[id];
-//        }
+        for (int i=0; i < items.length; i++) {
+            itemtracker.setId(id);
+            this.items[i] = this.items[id];
+            if ( ) {
+                return true;
+            }
+        }
         //должен заменить ячейку в массиве this.items.
         // Для этого необходимо найти ячейку в массиве по id.
         // Метод должен вернуть boolean результат - удалось ли провести операцию.
-        // на что заменить ячейку в массиве?
+
 
     }
     //        3-удаление заявок - public boolean delete(String id);
     public boolean delete(String id) {
+        for (int i=0; i < items.length; i++) {
+            ItemTracker deleteitem = new ItemTracker();
+            deleteitem.setId(id);
+            System.arraycopy(deleteitem, id, copydeleteitem, id-i, id);
+            if( ) {
+                return true;
+            }
+        }
+
         // должен удалить ячейку в массиве this.items. Для этого необходимо найти ячейку в массиве по id.
         // Далее сместить все значения справа от удаляемого элемента -
         // на одну ячейку влево с помощью System.arrayCopy().
         // Метод должен вернуть boolean результат - удалось ли провести операцию.
     };
+
+
     //        4-получение списка всех заявок - public Item[] findAll();
     public ItemTracker[] findAll() {
+        if (items != null) {
+            return ;
+            //как выглядит копия?
+        }
         // возвращает копию массива this.items без null элементов
     }
+
+
     //        5-получение списка по имени - public Item[] findByName(String key);
     public ItemTracker[] findByName(String key) {
+        for (int i=0; i < items.length; i++) {
+            ItemTracker itembyname = new ItemTracker();
+            itembyname.getName();
+
+        }
         // проверяет в цикле все элементы массива this.items,
         // сравнивая name (используя метод getName класса Item) с аргументом метода String key.
         // Элементы, у которых совпадает name, копирует в результирующий массив и возвращает его;
