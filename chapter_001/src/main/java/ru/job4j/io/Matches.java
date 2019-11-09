6f5985b134ef1d03ef57f19373da69c02e07fbc7
Anastasia Boldyreva <a93357@gmail.com>
2019-11-09 22:09:26 +0300

    Scanner и чтение числа из консоли. [#179518]

--- chapter_001/src/main/java/ru/job4j/io/Matches.java
+++ chapter_001/src/main/java/ru/job4j/io/Matches.java
@@ -4,26 +4,65 @@ import java.util.Scanner;
 
 public class Matches {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         boolean run = true;
         while (run) {
             System.out.println("Игра 11 спичек. Выигрывает тот, кто заберет последние спички. выберите от 1 до 3 спичек");
-            for (int i = 11; i > 0; i++) {
+            System.out.println("Введите число от 1 до 3");
+            int quantity = 11;
+            while (quantity > 0) {
                 int select = Integer.valueOf(input.nextLine());
-                int left = 11 - select*i;
+                int left = 11 - select;
                 if (left > 0) {
-                    if (select <=3) {
-                        System.out.println("Спасибо. Игра продолжается");
-                    } else {
-                        System.out.println(("Вы ввели не то число"));
+                    if (select == 1)  {
+                        System.out.println("Игра продолжается. Введите число от 1 до 3");
+                    } if (select == 2)  {
+                        System.out.println("Игра продолжается. Введите число от 1 до 3");
+                    } if (select == 3)  {
+                        System.out.println("Игра продолжается. Введите число от 1 до 3");
+                    } else if (select > 3) {
+                        System.out.println("Вы ввели число больше 3.");
                     }
                 }
-                else if (left <= 0 ) {
-                    System.out.println("Вы победили");
-                    break;
-                }
+
             }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//            for (int i = 11; i > 0; i++) {
+//                int select = Integer.valueOf(input.nextLine());
+//                int left = 11 - select*i;
+//                if (left > 0) {
+//                    if (select <=3) {
+//                        System.out.println("Спасибо. Игра продолжается");
+//                    } else {
+//                        System.out.println(("Вы ввели не то число"));
+//                    }
+//                }
+//                else if (left <= 0 ) {
+//                    System.out.println("Вы победили");
+//                    break;
+//                }
+//            }
 
 
 
@@ -65,7 +104,4 @@ public class Matches {
 //                System.out.println(("Вы ввели не то число"));
 //            }
 //            System.out.println();
-        }
-    }
-}
 
