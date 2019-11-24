d68e224dbafa11e97ca1c2d34ae5664fd5db39ef
Anastasia Boldyreva <a93357@gmail.com>
2019-11-24 09:51:12 +0300

    Scanner и чтение числа из консоли. [#179518]

--- chapter_001/src/main/java/ru/job4j/io/Matches.java
+++ chapter_001/src/main/java/ru/job4j/io/Matches.java
@@ -8,107 +8,34 @@ public class Matches {
         System.out.println("Игра 11 спичек. Выигрывает тот, кто заберет последние спички. выберите от 1 до 3 спичек");
         System.out.println("Введите число от 1 до 3");
         int quantity = 11;
-        while (quantity >= 0) {
-            for (int index = 0; index < 1; index++) {
+        boolean gamer = true;
+        while (quantity > 0) {
 
-                //устанавливаем очередь, с которой ходят игроки
-                if (index % 2 == 0) {
-                    System.out.println("Ходит игрок номер 1");
-                } else {
-                    System.out.println("Ходит игрок номер 2");
+            //устанавливаем очередь, с которой ходят игроки
+            if (gamer) {
+                System.out.println("Ходит игрок номер 1");
+            } else {
+                System.out.println("Ходит игрок номер 2");
+            }
+
+            //игроки вытаскивают по 1-3 спички
+            int select = Integer.parseInt(input.nextLine());
+            if (select > 0 && select < 4) {
+                quantity = quantity - select;
+                System.out.println("осталось" + quantity + "спичек");
+
+                // условие выигрыша
+                if (quantity <= 0) {
+                    if (gamer) {
+                        System.out.println("Победил игрок номер 1");
+                    } else {
+                        System.out.println("Победил игрок номер 2");
+                    }
                 }
 
-                //игроки вытаскивают 1-3 спички
-                int select = Integer.parseInt(input.nextLine());
-                if (select > 0 && select < 4) {
-                    quantity = quantity - select;
-                    System.out.println("осталось" + quantity + "спичек");
-                } else {
-                    System.out.println("Введите верное количество от 1 до 3");
-                }
-
-                // условие выигрыша не хватает условия
-                if (quantity <=0)  {
-                    System.out.println("win #1");
-                } else {
-                    System.out.println("win #2");
-                }
+            } else {
+                System.out.println("Введите верное количество от 1 до 3");
             }
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//            for (int i = 11; i > 0; i++) {
-//                int select = Integer.valueOf(input.nextLine());
-//                int left = 11 - select*i;
-//                if (left > 0) {
-//                    if (select <=3) {
-//                        System.out.println("Спасибо. Игра продолжается");
-//                    } else {
-//                        System.out.println(("Вы ввели не то число"));
-//                    }
-//                }
-//                else if (left <= 0 ) {
-//                    System.out.println("Вы победили");
-//                    break;
-//                }
-//            }
-
-
-
-
-//            for (int i = 1; i <11; i++) {
-//                int select = Integer.valueOf(input.nextLine());
-//                int left = 11 - select*i;
-//                if (left > 0) {
-//                    if (select <=3) {
-//                        System.out.println("Спасибо. Игра продолжается");
-//                    } else {
-//                        System.out.println(("Вы ввели не то число"));
-//                    }
-//                }
-
-
-
-
-
-//            System.out.println("Ходит игрок номер 1");
-//            System.out.println("выберите от 1 до 3 спичек");
-//            int select = Integer.valueOf(input.nextLine());
-//            if (select <=3) {
-//                System.out.println("Спасибо. Игра продолжается");
-//            } else {
-//                System.out.println(("Вы ввели не то число"));
-//            }
-//            if ((11 - select) <= 0){
-//                System.out.println(("Вы победили"));
-//                break;
-//            }
-
-//            System.out.println("Ходит игрок номер 2");
-//            System.out.println("выберите от 1 до 3 спичек");
-//            int selectTwo = Integer.valueOf(input.nextLine());
-//            if (selectTwo <=3) {
-//                System.out.println("Спасибо. Игра продолжается");
-//            } else {
-//                System.out.println(("Вы ввели не то число"));
-//            }
-//            System.out.println();
-
