5750307e42fea3d8009e6e464c123709b47130e9
Anastasia Boldyreva <a93357@gmail.com>
2019-11-25 17:52:38 +0300

    Реализовать класс Tracker [#396]

--- chapter_001/src/main/java/ru/job4j/Encapsulation/ItemTracker.java
+++ chapter_001/src/main/java/ru/job4j/Encapsulation/ItemTracker.java
@@ -4,23 +4,24 @@ public class ItemTracker {
     private String id;
     private String name;
 
-    public String getId() {
+    public ItemTracker (String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
+    public String getId() {
         return id;
     }
 
     public void setId(String id) {
-
         this.id = id;
     }
 
     public String getName() {
-
         return name;
     }
 
     public void setName(String name) {
-
         this.name = name;
     }
 
