2572ee09187011a3c5518f726a3888735d42681a
Anastasia Boldyreva <a93357@gmail.com>
2019-11-26 07:16:02 +0300

    Scanner и чтение числа из консоли. [#179518]

--- chapter_001/src/main/java/ru/job4j/io/Matches.java
+++ chapter_001/src/main/java/ru/job4j/io/Matches.java
@@ -8,34 +8,33 @@ public class Matches {
         System.out.println("Игра 11 спичек. Выигрывает тот, кто заберет последние спички. выберите от 1 до 3 спичек");
         System.out.println("Введите число от 1 до 3");
         int quantity = 11;
-        boolean gamer = true;
+        boolean gamer = false;
         while (quantity > 0) {
-
-            //устанавливаем очередь, с которой ходят игроки
+            //устанавливаем очеедь, с которой ходят игроки
             if (gamer) {
-                System.out.println("Ходит игрок номер 1");
-            } else {
                 System.out.println("Ходит игрок номер 2");
+            } else {
+                System.out.println("Ходит игрок номер 1");
             }
-
             //игроки вытаскивают по 1-3 спички
             int select = Integer.parseInt(input.nextLine());
             if (select > 0 && select < 4) {
                 quantity = quantity - select;
                 System.out.println("осталось" + quantity + "спичек");
-
-                // условие выигрыша
-                if (quantity <= 0) {
-                    if (gamer) {
-                        System.out.println("Победил игрок номер 1");
-                    } else {
-                        System.out.println("Победил игрок номер 2");
-                    }
-                }
-
+                gamer = ! gamer;
             } else {
                 System.out.println("Введите верное количество от 1 до 3");
             }
         }
+
+        // условие выигрыша
+        if (gamer) {
+            System.out.println("Победил игрок номер 1");
+        } else {
+            System.out.println("Победил игрок номер 2");
+        }
+
+
+
     }
 }
