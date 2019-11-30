a9b9d4f9c18b1bd8860d65079de81c91bd35e9fa
Anastasia Boldyreva <a93357@gmail.com>
2019-11-30 19:43:59 +0300

    Разрыв зависимости StartUI от Scanner. [#181778]

--- chapter_001/src/main/java/ru/job4j/Encapsulation/BreakDependency.java
+++ chapter_001/src/main/java/ru/job4j/Encapsulation/BreakDependency.java
@@ -5,49 +5,60 @@ public class BreakDependency {
         boolean run = true;
         while (run) {
             this.showMenu();
-            System.out.print("Select: ");
-            int select = Integer.valueOf(input.askStr(""));
+            int select = Integer.parseInt(input.askStr("Select: "));
             if (select == 0) {
                 System.out.println("=== Create a new Item ===");
                 String name = input.askStr("Enter name: ");
                 ItemTracker itemtracker = new ItemTracker(name);
                 tracker.add(itemtracker);
-            } else if(select == 1) {
+            } else if (select == 1) {
                 System.out.println("=== Show all items ===");
                 ItemTracker[] items = tracker.findAll();
-                for (ItemTracker itemTracker : items ) {
+                for (ItemTracker itemTracker : items) {
                     System.out.println(String.format("%s: %s", itemTracker.getId(), itemTracker.getName()));
                 }
-            } else if(select == 2) {
+            } else if (select == 2) {
                 System.out.println("=== Edit item ===");
                 String id = input.askStr("Enter id ");
                 String name = input.askStr("Enter name");
                 ItemTracker itemtracker = new ItemTracker(name);
-                tracker.replace(id, itemtracker);
+                if (tracker.replace(id, itemtracker)) {
+                    System.out.println(String.format("Item with id %s and name %s replaced", itemtracker.getId(), itemtracker.getName()));
+                } else {
+                    System.out.println("Something wrong happened. Pls recheck id and name.");
+                }
 
-            } else if(select == 3) {
+            } else if (select == 3) {
                 System.out.println("=== Delete item ===");
                 String id = input.askStr("Enter id");
-                tracker.delete(id);
+                if (tracker.delete(id)) {
+                    System.out.println(String.format("id %s deleted", id));
+                } else {
+                    System.out.println("Something wrong happened. Pls recheck id");
+                }
 
-            } else if(select == 4) {
+            } else if (select == 4) {
                 System.out.println("=== Find item by id ===");
                 String id = input.askStr("Enter id");
-                tracker.findById(id);
                 ItemTracker itemTracker = tracker.findById(id);
-                System.out.println(String.format("%s: %s", itemTracker.getId(), itemTracker.getName()));
+                if (tracker.findById(id) != null) {
+                    System.out.println(String.format("%s: %s", itemTracker.getId(), itemTracker.getName()));
+                } else {
+                    System.out.println("Something wrong happened. Pls recheck id");
+                }
 
 
-            } else if(select == 5) {
+
+            } else if (select == 5) {
                 System.out.println("=== Find items by name ===");
                 String name = input.askStr("Enter name");
-                tracker.findByName(name);
                 ItemTracker[] items = tracker.findByName(name);
-                for (ItemTracker itemTracker : items ) {
+                tracker.findByName(name);
+                for (ItemTracker itemTracker : items) {
                     System.out.println(String.format("%s: %s", itemTracker.getId(), itemTracker.getName()));
                 }
 
-            } else if(select == 6) {
+            } else if (select == 6) {
                 run = false;
             }
 
@@ -65,7 +76,7 @@ public class BreakDependency {
         System.out.println("Exit");
     }
 
-    public static void main (String[] args) {
+    public static void main(String[] args){
         Input input = new ConsoleInput();
         Tracker tracker = new Tracker();
         new BreakDependency().init(input, tracker);
