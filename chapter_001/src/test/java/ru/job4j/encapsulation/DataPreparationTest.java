7aacc17cbe7233c86887d14e31ec41581dc122be
Anastasia Boldyreva <a93357@gmail.com>
2019-12-01 08:55:16 +0300

    Тестирование. Подготовка данных. [#182960]

--- chapter_001/src/test/java/ru/job4j/encapsulation/DataPreparationTest.java
+++ chapter_001/src/test/java/ru/job4j/encapsulation/DataPreparationTest.java
@@ -11,7 +11,7 @@ public class DataPreparationTest {
         Tracker tracker = new Tracker();
         ItemTracker itemtracker = new ItemTracker("new item");
         tracker.add(itemtracker);
-        String[] answers = { itemtracker.getId(), "replaced item"};
+        String[] answers = {itemtracker.getId(), "replaced item"};
         StaticMethod.editItem(new StubInputStatic(answers), tracker);
         ItemTracker replaced = tracker.findById(itemtracker.getId());
         assertThat(replaced.getName(),is("replaced item"));
@@ -26,6 +26,18 @@ public class DataPreparationTest {
         StaticMethod.deleteItem(new StubInputStatic(answers), tracker);
         ItemTracker ex = tracker.findById(add.getId());
         assertNull(ex);
+
+    }
+
+    @Test
+    public void whenFindItem() {
+        Tracker tracker = new Tracker();
+        ItemTracker itemtracker = new ItemTracker("find Item");
+        tracker.add(itemtracker);
+        String[] answers = {itemtracker.getId(), "this item found"};
+        StaticMethod.findById(new StubInputStatic(answers), tracker);
+        ItemTracker found = tracker.findById(itemtracker.getId());
+        assertThat(found.getName(), is("this item found"));
 
     }
 
