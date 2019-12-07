83f88894dd1d89aeab4fe8612cf3a72afc86cb2f
Anastasia Boldyreva <a93357@gmail.com>
2019-12-07 20:05:02 +0300

    Шаблон проектирования - Стратегия. [#786]

--- chapter_001/src/test/java/ru/job4j/strategy/PaintTest.java
+++ chapter_001/src/test/java/ru/job4j/strategy/PaintTest.java
@@ -10,7 +10,23 @@ public class PaintTest {
 
     @Test
     public void whenDrawSquare() {
-
+        //получаем ссылку на стандартный вывод в консоль
+        PrintStream stdout = System.out;
+        //создаем буфер для хранения вывода
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        //заменяем стандартный вывод на вывод в память для тестирования
+        System.setOut(new PrintStream(out));
+        //выполняем действие, которое пишет в консоль
+        new Paint().draw(new Square());
+        //проверяем результат вычисления
+        assertThat(new String(out.toByteArray()), is (
+                new StringBuilder()
+                        .append("++++" + System.lineSeparator())
+                        .append("++++" + System.lineSeparator())
+                        .append("++++" + System.lineSeparator())
+                        .append("++++" + System.lineSeparator())
+                        .toString()));
+        //возвращаем обратно стандартный вывод в консоль
+        System.setOut(stdout);
     }
-
 }
