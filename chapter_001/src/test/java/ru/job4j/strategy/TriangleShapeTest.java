e5bd13ea42ddd3ca3ae29c6dbe695e5f0782df4b
Anastasia Boldyreva <a93357@gmail.com>
2019-12-08 19:02:37 +0300

    Шаблон проектирования - Стратегия. [#786]

--- chapter_001/src/test/java/ru/job4j/strategy/TriangleShapeTest.java
+++ chapter_001/src/test/java/ru/job4j/strategy/TriangleShapeTest.java
@@ -6,15 +6,21 @@ import static org.junit.Assert.assertThat;
 
 public class TriangleShapeTest {
 
+    private static final String NL = System.lineSeparator();
+
     @Test
     public void whenDrawTriangle() {
         TriangleShape triangleShape = new TriangleShape();
         assertThat(triangleShape.draw(), is(
                 new StringBuilder()
                         .append("+")
+                        .append(NL)
                         .append("++")
+                        .append(NL)
                         .append("+++")
+                        .append(NL)
                         .append("++++")
+                        .append(NL)
                         .toString()
                 )
         );
