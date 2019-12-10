1a84397275b9704af7c9d2ab1a49a68f6de033f1
Anastasia Boldyreva <a93357@gmail.com>
2019-12-10 21:42:49 +0300

    Рефакторинг теста @Before @After [#33568]

--- chapter_001/src/test/java/ru/job4j/strategy/TriangleShapeTest.java
+++ chapter_001/src/test/java/ru/job4j/strategy/TriangleShapeTest.java
@@ -20,7 +20,7 @@ public class TriangleShapeTest {
                         .append("+++")
                         .append(NL)
                         .append("++++")
-                        .append(NL)
+                        //.append(NL)
                         .toString()
                 )
         );
