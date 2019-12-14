2ec876a309fc6d30193a99adbc52d8b7c4260bea
Anastasia Boldyreva <a93357@gmail.com>
2019-12-14 20:57:56 +0300

    Рефакторинг - Шаблон Декоратор для валидатора. [#34117]

---
+++ chapter_002/src/test/java/ru.job4j/encapsulation/ValidateInputTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.encapsulation;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+
+public class ValidateInputTest {
+
+    @Test
+    public void whenInvalidInput() {
+        ByteArrayOutputStream mem = new ByteArrayOutputStream();
+        PrintStream out = System.out;
+        System.setOut(new PrintStream(mem));
+        ValidateInput input = new ValidateInput(
+                new StubInputStatic(new String[] {"invalid", "1"})
+        );
+        input.askInt("Enter", 1);
+        assertThat(
+                mem.toString(),
+                is(String.format("Please enter validate data again.%n"))
+        );
+        System.setOut(out);
+    }
+}
