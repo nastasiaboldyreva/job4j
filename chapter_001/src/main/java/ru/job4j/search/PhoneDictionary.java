fc5cdc098784ace2f7bf74f3800cd552e65aeac8
Anastasia Boldyreva <a93357@gmail.com>
2019-12-21 14:59:07 +0300

    Телефонный справочник на базе ArrayList [#41598]

---
+++ chapter_001/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,20 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList<Person>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+
+
+
+    public ArrayList<Person> find(String key) {
+        ArrayList<Person> result = new ArrayList<>();
+        return result;
+    }
+
+}
