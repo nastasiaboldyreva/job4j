5bb4f8c96ac78c51c2432ffc75d31f0252ca9eee
Anastasia Boldyreva <a93357@gmail.com>
2019-12-24 23:25:17 +0300

    Очередь с приоритетом на LinkedList [#41670]

--- chapter_001/src/main/java/ru/job4j/search/PriorityQueue.java
+++ chapter_001/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -14,7 +14,8 @@ public class PriorityQueue {
     public void put(Task task) {
         int index = 0;
         for(Task element : tasks) {
-            task.add(element.getPriority(), element.getDesc());
+            element = tasks.get(index);
+            tasks.set(index, element);
         } this.tasks.add(index, task);
 
     }
