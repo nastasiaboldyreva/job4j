09cfb9d69466c53bdf945532e8f055cbd9ab6ced
Anastasia Boldyreva <a93357@gmail.com>
2019-12-31 14:44:26 +0300

    Уникальные задачи. [#212653]

--- chapter_001/src/main/java/ru/job4j/collection/FullSearch.java
+++ chapter_001/src/main/java/ru/job4j/collection/FullSearch.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class FullSearch {
     public static HashSet<String> extractNumber(List<Task> list) {
         HashSet<String> numbers = new HashSet<>();
-        for(String duplicate : list) {
+        for(Task duplicate : list) {
             duplicate.equals(list);
         }
         return numbers;
