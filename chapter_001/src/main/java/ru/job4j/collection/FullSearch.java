42499b1e3742d2314eba4ab1ba0daab6ea17512e
Anastasia Boldyreva <a93357@gmail.com>
2020-01-02 16:49:00 +0300

    Уникальные задачи. [#212653]

--- chapter_001/src/main/java/ru/job4j/collection/FullSearch.java
+++ chapter_001/src/main/java/ru/job4j/collection/FullSearch.java
@@ -3,14 +3,15 @@ package ru.job4j.collection;
 import java.util.HashSet;
 import java.util.List;
 
-
+//нужно убрать дубликаты из списка, полученного от системы.
+//FullSearch - класс, который извлечен все номера и уберет дубликаты.
 public class FullSearch {
     public static HashSet<String> extractNumber(List<Task> list) {
         HashSet<String> numbers = new HashSet<>();
         for(Task duplicate : list) {
-            duplicate.equals(list);
-
+            numbers.add(duplicate.getNumber());
         }
+
         return numbers;
     }
 
