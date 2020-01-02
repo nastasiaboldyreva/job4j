a098504c0fa049eae788247b03f87b1a65378149
Anastasia Boldyreva <a93357@gmail.com>
2020-01-02 18:32:06 +0300

    Map, HashMap [#212652]

---
+++ chapter_001/src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,17 @@
+package ru.job4j.collection;
+
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        //почта - key, фио - value
+        map.put("a93357@gmail.com", "Boldyreva Anastasia Sergeevna");
+        for(String key : map.keySet()){
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+
+    }
+}
