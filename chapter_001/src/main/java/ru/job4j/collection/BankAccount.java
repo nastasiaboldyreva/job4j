e3b3f2b50bbb07ebe340ab1c6a7877c23ab799aa
Anastasia Boldyreva <a93357@gmail.com>
2020-01-11 20:29:40 +0300

    Банковские переводы. [#10038]

---
+++ chapter_001/src/main/java/ru/job4j/collection/BankAccount.java
@@ -0,0 +1,43 @@
+package ru.job4j.collection;
+
+import java.util.Objects;
+
+public class BankAccount {
+
+    private String requisite;
+    private double balance;
+
+    public BankAccount(String requisite, double balance) {
+        this.requisite = requisite;
+        this.balance = balance;
+    }
+
+    public String getRequisite() {
+        return requisite;
+    }
+
+    public void setRequisite(String requisite) {
+        this.requisite = requisite;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BankAccount account = (BankAccount) o;
+        return Objects.equals(requisite, account.requisite);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(requisite);
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/collection/BankUser.java
@@ -0,0 +1,44 @@
+package ru.job4j.collection;
+
+import java.util.Objects;
+
+public class BankUser {
+
+    private String passport;
+    private String username;
+
+    public BankUser(String passport, String username) {
+        this.passport = passport;
+        this.username = username;
+    }
+
+    public String getPassport() {
+        return passport;
+    }
+
+    public void setPassport(String passport) {
+        this.passport = passport;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BankUser user = (BankUser) o;
+        return Objects.equals(passport, user.passport);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(passport);
+    }
+
+}
