04f581f350a7cc8bd1f69ff9312b7174c7141c90
Anastasia Boldyreva <a93357@gmail.com>
2020-01-15 23:04:54 +0300

    Сортировка [#10096]

---
+++ chapter_002/src/main/java/ru/job4j/encapsulation/ItemTrackerSort.java
@@ -0,0 +1,10 @@
+package ru.job4j.encapsulation;
+
+import java.util.Comparator;
+
+public class ItemTrackerSort implements Comparator<ItemTrackerSort> {
+    @Override
+    public int compare(ItemTrackerSort o1, ItemTrackerSort o2) {
+        return 0;
+    }
+}
