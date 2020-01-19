f9c52dc04d48c7d7ecc2f7fb4abe1383f8a1be22
Anastasia Boldyreva <a93357@gmail.com>
2020-01-19 22:18:03 +0300

    Организовать сортировку User [#10034]

---
+++ chapter_001/src/main/java/ru/job4j/collection/User.java
@@ -0,0 +1,37 @@
+package ru.job4j.collection;
+
+import java.util.Objects;
+
+public class User implements Comparable<User> {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    //compareTo должен сравнить имена, если они равны, то сравнить возраст.
+    @Override
+    public int compareTo(User o) {
+        int rsl = this.name.compareTo(o.name);
+        if(rsl == 0) {
+            rsl = this.age.compareTo(o.age);
+        } return rsl;
+    }
+
+    //методы equals и hashCode нужны для тестов, где мы используем методы сравнения моделей user.equals.
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age &&
+                name.equals(user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
