dd1527391f273852eaa0beac933d60207c2c88b4
Anastasia Boldyreva <a93357@gmail.com>
2020-02-01 21:14:20 +0300

    Отсортировать департаменты [#34352]

--- chapter_001/src/main/java/ru/job4j/collection/Departments.java
+++ chapter_001/src/main/java/ru/job4j/collection/Departments.java
@@ -1,42 +1,46 @@
-//package ru.job4j.collection;
-//
-//import java.util.ArrayList;
-//import java.util.HashSet;
-//import java.util.List;
-//
-//public class Departments {
-//
-//    public static List<String> fillGaps(List<String> deps) {
-//        List<String> rsl = new ArrayList<>();
-//        return rsl;
-//
-///**
-// * HashSet allows to eliminate duplicates
-// * which may be result of sequent individual departments summing
-// */
-//    //Чтобы реализовать метод fillGaps, нужно определить,
-//    // какие элементы отсутствуют в системе.
-//    // Для этого каждую входящую строку нужно разбить на одиночные элементы.
-//    // Для этого можно использовать метод String.split("/").
-//    // Этот метод вернет массив одиночных элементов.
-//    // Далее нужно через цикл последовательно складывать элементы и добавлять из в множество.
-//    // Используйте в качестве промежуточного хранения HashSet.
-//
-//    HashSet<String> tmp = new HashSet<>();
-//    for (String value : deps) {
-//        String start = " ";
-//        for (String el : value.split("/")) {
-//            // tmp.add start + "/" + el
-//        }
-//    } return new ArrayList<>(tmp);
-//    }
-//
-//
-//    public static void sortAsc(List<String> orgs) {
-//
-//    }
-//
-//    public static void sortDest(List<String> orgs) {
-//
-//    }
-//}
+package ru.job4j.collection;
+
+import javax.sound.midi.Soundbank;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class Departments {
+
+    public static List<String> fillGaps(List<String> deps) {
+        List<String> rsl = new ArrayList<>();
+        return rsl;
+
+        /**
+         * HashSet allows to eliminate duplicates
+         * which may be result of sequent individual departments summing
+         */
+        // Чтобы реализовать метод fillGaps, нужно определить,
+        // какие элементы отсутствуют в системе.
+        // Для этого каждую входящую строку нужно разбить на одиночные элементы.
+        // Для этого можно использовать метод String.split("/").
+        // Этот метод вернет массив одиночных элементов.
+        // Далее нужно через цикл последовательно складывать элементы и добавлять из в множество
+        // Используйте в качестве промежуточного хранения HashSet.
+
+        HashSet<String> tmp = new HashSet<>();
+        for (String value : deps) {
+            String start = " ";
+            for (String el : value.split("/")) {
+                tmp.add(start + "/" + el);
+
+            }
+        } return new ArrayList<>(tmp);
+    }
+
+
+
+
+    public static void sortAsc(List<String> orgs) {
+
+    }
+
+    public static void sortDest(List<String> orgs) {
+
+    }
+}
