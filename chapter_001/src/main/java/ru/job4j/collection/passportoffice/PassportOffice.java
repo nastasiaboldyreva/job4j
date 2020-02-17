d0a60d099c4a844ccd3f02b6967302db22cde50b
Anastasia Boldyreva <a93357@gmail.com>
2020-02-17 22:09:54 +0300

    Паспорт и жители. [#212656]

---
+++ chapter_001/src/main/java/ru/job4j/collection/passportoffice/PassportOffice.java
@@ -0,0 +1,30 @@
+package ru.job4j.collection.passportoffice;
+
+import ru.job4j.collection.passportoffice.Citizen;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/** passport office system
+ * people come and request their passports
+ **/
+
+
+public class PassportOffice {
+    private Map<String, Citizen> citizens = new HashMap<>();
+    public boolean add(Citizen citizen) {
+        boolean rsl = false;
+        if (!citizens.containsKey(citizen.getPassport())) {
+            citizens.put(citizen.getPassport(), citizen);
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    public Citizen get(String passport) {
+
+        return citizens.get(passport);
+    }
+}
+
+
