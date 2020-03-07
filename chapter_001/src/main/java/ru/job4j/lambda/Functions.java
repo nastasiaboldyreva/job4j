bd76fa1ee598a70465f2aca456461a3d3c952433
Anastasia Boldyreva <a93357@gmail.com>
2020-03-07 20:27:39 +0300

    Подсчет функции в диапазоне. [#24251]

--- chapter_001/src/main/java/ru/job4j/lambda/Functions.java
+++ chapter_001/src/main/java/ru/job4j/lambda/Functions.java
@@ -5,10 +5,10 @@ import java.util.List;
 import java.util.function.Function;
 
 public class Functions {
-    List<Double> diapason(int start, int end, Function<Double, Double> func) {
+    List<Double> diapason (int start, int end, Function<Double, Double> func) {
         List<Double> result = new ArrayList<>();
         for (int index = start; index != end; index++) {
-            result.add(func.apply((double)index);
+            result.add(func.apply((double)index));
         }
         return result;
     }
--- chapter_001/src/test/java/ru/job4j/lambda/FunctionsTest.java
+++ chapter_001/src/test/java/ru/job4j/lambda/FunctionsTest.java
@@ -1,20 +1,36 @@
 package ru.job4j.lambda;
 
-        import org.junit.Test;
-        import java.util.ArrayList;
-        import java.util.Arrays;
-        import java.util.List;
-        import java.util.function.Function;
-        import static org.hamcrest.Matchers.is;
-        import static org.junit.Assert.assertThat;
+import org.junit.Test;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
-public class FunctionCalcTest {
+public class FunctionsTest {
 
     @Test
     public void whenLinearFunctionThenLinearResults() {
+        Function function = new Function();
         List<Double> result = function.diapason(5, 8, x -> 2 * x + 1);
         List<Double> expected = Arrays.asList(11D, 13D, 15D);
         assertThat(result, is(expected));
     }
 
+    @Test
+    public void whenQuadraticFunction() {
+        Function function = new Function();
+        List<Double> result = function.diapason(5, 8, x -> 3 * x * x + 2 * x + 1);
+        List<Double> expected = Arrays.asList(86D, 121D, 162D);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenLogarithmicFunction() {
+        Function function = new Function();
+        List<Double> result = function.diapason(5, 8, x -> Math.log(x));
+        List<Double> expected = Arrays.asList(1.609D, 1.792D, 1.946D);
+        assertThat(result, is(expected));
+    }
 }
