1e4f5dba4e5fcdb3f9dc6d3dd75a430664533a5a
Anastasia Boldyreva <a93357@gmail.com>
2020-03-08 09:49:29 +0300

    Функции высшего порядка [#24255]

--- chapter_001/src/main/java/ru/job4j/collection/job/JobSorter.java
+++ chapter_001/src/main/java/ru/job4j/collection/job/JobSorter.java
@@ -2,6 +2,7 @@ package ru.job4j.collection.job;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class JobSorter {
@@ -14,6 +15,12 @@ public class JobSorter {
 
         Collections.sort(jobs, new JobDescByName().thenComparing(new JobDescByPriority()));
         System.out.println(jobs);
+
+        Comparator<Job> compareName = Comparator.comparing(Job::getName);
+        Comparator<Job> comparePriority = Comparator.comparingInt(Job::getPriority);
+        Comparator<Job> combine = compareName.thenComparing(comparePriority);
+
+        jobs.sort(combine);
     }
 }
 
--- chapter_001/src/main/java/ru/job4j/search/PhoneDictionary.java
+++ chapter_001/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,6 +1,8 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.function.Predicate;
 
 public class PhoneDictionary {
     private ArrayList<Person> persons = new ArrayList<>();
@@ -16,17 +18,31 @@ public class PhoneDictionary {
      * @param key Ключ поиска.
      * @return Список подощедщих пользователей.
      */
+//    public ArrayList<Person> find(String key) {
+//        ArrayList<Person> result = new ArrayList<>();
+//        for (Person person : persons) {
+//            if (person.getSurname().contains(key)
+//                    || person.getName().contains(key)
+//                    || person.getAddress().contains(key)
+//                    || person.getPhone().contains(key)) {
+//                result.add(person);
+//            }
+//        }
+//        return result;
+//    }
+
     public ArrayList<Person> find(String key) {
+        Predicate<Person> findSurname = Predicate.isEqual(Person::getSurname);
+        Predicate<Person> findName = Predicate.isEqual(Person::getName);
+        Predicate<Person> findAddress = Predicate.isEqual(Person::getAddress);
+        Predicate<Person> findPhone = Predicate.isEqual(Person::getPhone);
+        Predicate<Person> combine =  ;
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
-            if (person.getSurname().contains(key)
-                    || person.getName().contains(key)
-                    || person.getAddress().contains(key)
-                    || person.getPhone().contains(key)) {
+            if (combine.test(person)) {
                 result.add(person);
             }
         }
         return result;
     }
-
 }
