ead2da6263219ae050af1f2baad7d9e03237e917
Anastasia Boldyreva <a93357@gmail.com>
2020-03-11 22:11:09 +0300

    Список адресов [#110062]

--- chapter_001/src/main/java/ru/job4j/streamapi/AddressesList.java
+++ chapter_001/src/main/java/ru/job4j/streamapi/AddressesList.java
@@ -6,6 +6,6 @@ import java.util.List;
 public class AddressesList {
 
     List<Address> collect(List<Profile> profiles) {
-        profiles.stream().map(Profile::getAddress).collect(Collectors.toList());
+        return profiles.stream().map(Profile::getAddress).collect(Collectors.toList());
     }
 }
