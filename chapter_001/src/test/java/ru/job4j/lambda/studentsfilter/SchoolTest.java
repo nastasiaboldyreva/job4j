a1bb4a2980c882bbdabd89c1cf1db1387596b79f
Anastasia Boldyreva <a93357@gmail.com>
2020-03-11 22:50:51 +0300

    Фильтрация учеников. [#110058]

--- chapter_001/src/test/java/ru/job4j/lambda/studentsfilter/SchoolTest.java
+++ chapter_001/src/test/java/ru/job4j/lambda/studentsfilter/SchoolTest.java
@@ -10,10 +10,16 @@ public class SchoolTest {
     @Test
     public void filter10AClass() {
         School school = new School();
+        List<Student> students = List.of(
+               new Student(90),
+               new Student(80),
+               new Student(70),
+               new Student(60)
+        );
         List<Student> result = school.collect(
-                students, students -> students.getScore() <= 100);
+                students, p -> p.getScore() <= 100);
         List<Student> expected = school.collect(
-                students, students -> students.getScore() >= 70 && students.getScore() <= 100);
+                students, p -> p.getScore() >= 70 && p.getScore() <= 100);
         assertThat(result, is(expected));
     }
 
@@ -23,10 +29,16 @@ public class SchoolTest {
     @Test
     public void filter10BClass() {
         School school = new School();
+        List<Student> students = List.of(
+                new Student(75),
+                new Student(65),
+                new Student(55),
+                new Student(45)
+        );
         List<Student> result = school.collect(
-                students, students -> students.getScore() <= 100);
+                students, p -> p.getScore() <= 100);
         List<Student> expected = school.collect(
-                students, students -> students.getScore() >= 50 && students.getScore() <= 70);
+                students, p -> p.getScore() >= 50 && p.getScore() <= 70);
         assertThat(result, is(expected));
     }
 
@@ -34,10 +46,16 @@ public class SchoolTest {
     @Test
     public void filter10CClass() {
         School school = new School();
+        List<Student> students = List.of(
+                new Student(60),
+                new Student(50),
+                new Student(40),
+                new Student(30)
+        );
         List<Student> result = school.collect(
-                students, students -> students.getScore() <= 100);
+                students, p -> p.getScore() <= 100);
         List<Student> expected = school.collect(
-                students, students -> students.getScore() >= 0 && students.getScore() <= 50);
+                students, p -> p.getScore() >= 0 && p.getScore() <= 50);
         assertThat(result, is(expected));
     }
 }
