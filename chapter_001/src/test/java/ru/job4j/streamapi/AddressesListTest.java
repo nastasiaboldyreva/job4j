f70b12005a9892bd5341a4b64f5850f809f3797e
Anastasia Boldyreva <a93357@gmail.com>
2020-06-03 21:25:17 +0300

    Список адресов [#110062]

---
+++ chapter_001/src/test/java/ru/job4j/streamapi/AddressesListTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.streamapi;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class AddressesListTest {
+
+    @Test
+    public void collect() {
+        AddressesList addressesList = new AddressesList();
+        List<Profile> result = ;
+        List<Profile> expected = ;
+        assertThat(result, is(expected));
+    }
+}
